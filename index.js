337ab325be94f7d45e9b397b4cdf5125a0d17ec1
Reihannnn <areihanahmad@gmail.com>
2024-07-28 16:28:26 +0700

    homepage is already clear

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+const myApiKey = "d89d0a2da42a4b34bcf53ed802d28c4f";
+const app = express();
+const port = 3000;
+const url = `https://newsapi.org/v2/`;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  const responseEverything = await axios.get(url + "everything", {
+    params: {
+      q: `keyword`,
+      apiKey: myApiKey,
+    },
+  });
+
+  const responseTopHeadlines = await axios.get(url + "top-headlines/", {
+    params: {
+      country: "id",
+      apiKey: myApiKey,
+    },
+  });
+
+  const resultEverything = responseEverything.data;
+  const resultTopHeadlines = responseTopHeadlines.data;
+
+  function getRandomIndex(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+    // inisialisi random yang value nya 
+  const randomResultEverything = getRandomIndex(resultEverything.articles);
+  // const randomResultTopHeadlines = getRandomIndex(
+  //   resultTopHeadlines.articles
+  // );   ==> random topHeadlines
+  const randomResultEverything2 = getRandomIndex(resultEverything.articles);
+
+  // variable min n max
+  const max =  resultTopHeadlines.articles.length - 4 
+  const min =  0
+
+  // variable rumus mathrandom 
+  const mathRandom = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  console.log(resultEverything)
+  console.log(resultTopHeadlines)
+
+  res.render("index.ejs", {
+    contentEverythingNews: randomResultEverything,
+    contentEverythingNews2 : randomResultEverything2,
+    contentTopHeadlines: resultTopHeadlines.articles,
+    mathRandom : mathRandom
+  });
+});
+
+app.listen(port, () => {
+  try {
+    console.log(`your server is run in localhost:${port}`);
+  } catch (error) {
+    console.log(`your error in : ${error.message}`);
+  }
+});
